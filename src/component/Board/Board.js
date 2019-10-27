ae755d27858f7eb30f61ddd7794053aa04f72563
Iino Ten <nteten0313@icloud.com>
2019-10-27 14:50:38 +0900

    盤面のデザインを追加

--- src/component/Board/Board.js
+++ src/component/Board/Board.js
@@ -2,13 +2,17 @@ import React, {Component} from 'react';
 import {isMobile} from 'react-device-detect'
 
 import Chip from '../Chip/Chip'
+import Frame from '../Frame/Frame'
 
 import './Board.css'
 
 class Board extends Component{
   render(){
     return(
-      <div id={isMobile?"board-mobile":"board-pc"}><Chip /></div>
+      <div id={isMobile?"board-mobile":"board-pc"}>
+        <Frame />
+        <Chip />
+      </div>
     );
   }
 }
