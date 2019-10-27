5ea451dc9020110617d3f6fc5ac4ec65d62e5775
Iino Ten <nteten0313@icloud.com>
2019-10-27 14:51:00 +0900

    デザインの変更

---
+++ src/component/Frame/Frame.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+import './Frame.css'
+
+class Frame extends Component{
+  render(){
+    return(
+      <div>
+        <table>
+          <tbody>
+          <tr>
+            <td /><td /><td /><td /><td /><td />
+          </tr>
+          <tr>
+            <td /><td /><td /><td /><td /><td />
+          </tr>
+          <tr>
+            <td /><td /><td /><td /><td /><td />
+          </tr>
+          <tr>
+            <td /><td /><td /><td /><td /><td />
+          </tr>
+          <tr>
+            <td /><td /><td /><td /><td /><td />
+          </tr>
+          <tr>
+            <td /><td /><td /><td /><td /><td />
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Frame;
