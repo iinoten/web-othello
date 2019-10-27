fbb23aa1a56d81b5cb3920918485332213083692
Iino Ten <nteten0313@icloud.com>
2019-10-27 18:25:14 +0900

    Draggable要素を排除、styleを変化するように

--- src/component/Chip/Chip.js
+++ src/component/Chip/Chip.js
@@ -1,23 +1,40 @@
-import React, {Component} from 'react';
-
-import './Chip.css'
+import React, { Component } from 'react';
 import Draggable from 'react-draggable';
+import posed from 'react-pose';
+import './Chip.css';
+
+const rand_percent = () => {
+  return Math.floor(Math.random () * 50) + "% " + Math.floor(Math.random () * 50) + "% " + Math.floor(Math.random () * 50) + "% " + Math.floor(Math.random () * 50) + "%/" + Math.floor(Math.random () * 50) + "% " + Math.floor(Math.random () * 50) + "% " + Math.floor(Math.random () * 50) + "% " + Math.floor(Math.random () * 50) + "%";
+}
+
+const Box = posed.div({
+  right: {borderRadius: rand_percent},
+  left:  {borderRadius: rand_percent}
+})
 
 class Chip extends Component{
   constructor(){
     super();
     this.state = {
-      is_black: true
+      is_black: true,
+      style_flag: true
     }
   }
+
   doubletap_handler = () => {
     this.setState({is_black: !this.state.is_black})
   }
+  
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({style_flag: !this.state.style_flag})
+    },500);
+  }
+
   render(){
     return(
-      <Draggable>
-        <div onDoubleClick={this.doubletap_handler} className={"chip " + (this.state.is_black?"black":"white")}></div>
-      </Draggable>
+        <Box className="box" pose={this.state.style_flag ? 'right' : 'left'} className={"chip " + (this.state.is_black?"black":"white")}>
+        </Box>
     );
   }
 }
